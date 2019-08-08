a7b3f9b5c2e5f24d6ab0116d83e899047fe82ab2
Daniil Polyakov <danfromomsk@gmail.com>
2019-08-08 22:05:45 +0600

    typo fix

--- src/main/kotlin/com/danpolyakov/telegram_bots/secret_hitler_moderator/Symbols.kt
+++ src/main/kotlin/com/danpolyakov/telegram_bots/secret_hitler_moderator/Symbols.kt
@@ -3,7 +3,7 @@ package com.danpolyakov.telegram_bots.secret_hitler_moderator
 enum class Symbols(private val str : String, val description : String) {
 
     EMPTY_FIELD("\u25FB️️️️", "Empty field"),
-    COVERED_FIELD("\u274C\uFE0F", "Field covered ith a card"),
+    COVERED_FIELD("\u274C\uFE0F", "Field covered with a card"),
     POLICY_PEEK("\uD83D\uDD2E", "Presidential Power: Policy Peek"),
     INVEST("\uD83D\uDD0D", "Presidential Power: Investigate Loyalty"),
     EXECUTE("\uD83D\uDDE1", "Presidential Power: Execution"),
